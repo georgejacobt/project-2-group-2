f6cb19e73d4390bee72c3407a647aa566a687ba9
georgejacobt <36488006+georgejacobt@users.noreply.github.com>
2018-08-21 10:18:33 -0500

    working twillo sms

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 module.exports = function(app) {
   // Get all examples
@@ -9,10 +10,38 @@ module.exports = function(app) {
   });
 
   // Twillo Post
+  var counter = 0;
   app.post("/sms", (req, res) => {
     const twiml = new MessagingResponse();
+    let from = req.body.From;
+    let body = req.body.Body;
+    console.log (from);
+    console.log(body);
 
-    twiml.message("The Robots are coming! Head for the hills!");
+    if (counter === 0){
+      twiml.message("Hi, Welcome! Are you looking for pet care , home sitting or both..?");
+      counter = counter +1;
+    }
+    else {
+      if (counter === 1 & body === "pet care"){
+        twiml.message("Great, we offer excellent pet care options, woould you like for sam to call you back for a meet and greet..");
+        counter =  counter + 1;
+      }
+
+    }
+      if (counter === 2 & body === "yes" ){
+        twiml.message("OK, I wil have Sam call you back at "+from);
+        counter =  counter + 1;
+        }
+      
+    console.log(counter);
+
+    
+
+    
+
+   
+
 
     res.writeHead(200, { "Content-Type": "text/xml" });
     res.end(twiml.toString());
@@ -42,4 +71,19 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+// app.post('/sms', (req, res) => {
+//   const twiml = new MessagingResponse();
+
+//   twiml.message('Hi, Welcome! Are you looking for pet care , home sitting or both..?');
+
+//   res.writeHead(200, {'Content-Type': 'text/xml'});
+//   res.end(twiml.toString());
+// });
+
+
+
 };
+
+
+
