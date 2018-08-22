965dd5803cc27f79fba263535e61f031e2a0899c
Dan Hopper <dhopper2@sbcglobal.net>
2018-08-22 17:23:22 -0500

    Started creating revenue list (payments)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/examples", function (req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
@@ -42,6 +42,36 @@ module.exports = function(app) {
   app.post("/api/user", function(req, res) {
     db.User.create(req.body).then(function(dbUsers) {
       res.json(dbUsers);
+    });
+  });
+  //Get completed payments
+  app.get("/api/revenue", function(req, res) {
+    db.Appointment.findAll({ where: { complete: 1 } }).then(function(
+      dbAppointment
+    ) {
+      let payedPets = [];
+      let petTotal = 0;
+      let payedHomes = [];
+      let homeTotal = 0;
+      for (let i = 0; i < dbAppointment.length; i++) {
+        if (dbAppointment[i].type === false) {
+          payedPets.push(dbAppointment[i]);
+          petTotal += parseFloat(dbAppointment[i].rate, 11);
+        } else {
+          payedHomes.push(dbAppointment[i]);
+          homeTotal += parseFloat(dbAppointment[i].rate, 11);
+        }
+      }
+      let obj = {
+        pets: payedPets,
+        homes: payedHomes,
+        petTotal: petTotal,
+        homeTotal: homeTotal
+      };
+      res.render("payments", obj);
+      // for (let i = 0; i < dbAppointment.length; i++) {
+      //   console.log(JSON.stringify(dbAppointment[i]));
+      // }
     });
   });
 };
