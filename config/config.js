e0f55c72f83c62d35bd852502f46e7b24a73df0f
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-08-21 21:20:52 -0500

    heroku dpeloy

--- config/config.js
+++ config/config.js
@@ -17,19 +17,3 @@ module.exports = {
     "dialect": "mysql"
   }
 };
-
-// require("dotenv").config();
-
-// module.exports = {
-//   development: {
-//     database: process.env.DB_NAME,
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: "mysql",
-//     define: {
-//       underscored: true
-//     }
-//   }
-// };
